ede028c6a4ec5b06f1ec368abc82a749fd0c1884
Suxsem <suxsem@live.com>
2015-02-27 02:06:30 +0100

    Fixed indentation
    Signed-off-by: Suxsem <suxsem@live.com>

--- mqttduino.ino
+++ mqttduino.ino
@@ -1,28 +1,28 @@
-#define APssid				"HomeWIFI"				//wifi network ssid
-#define APpsw				"wifipassword"			//wifi netwrok password
-#define MQTTid				"MqttTestClient"		//id of this mqtt client
-#define MQTTip				"mqtt.myserver.com"		//ip address or hostname of the mqtt broker
-#define MQTTport			1883					//port of the mqtt broker
-#define MQTTuser			"client1"				//username of this mqtt client
-#define MQTTpsw				"password1"				//password of this mqtt client
-#define MQTTalive			120						//mqtt keep alive interval (seconds)
-#define MQTTretry			10						//time to wait before reconnect if connection drops (seconds)
-#define MQTTqos				2						//quality of service for subscriptions and publishes
-#define timeout_check		500						//timeout for checking incoming messages (milliseconds)
-#define timeout_send		5000					//timeout for subscriptions and publishes (milliseconds)
-#define esp8266reset		A5						//arduino pin connected to esp8266 reset pin (analog pin suggested due to higher impedance)
-#define esp8266alive		40						//esp8266 keep alive interval (reset board if fail) (seconds)
+#define APssid              "HomeWIFI"              //wifi network ssid
+#define APpsw               "wifipassword"          //wifi netwrok password
+#define MQTTid              "MqttTestClient"        //id of this mqtt client
+#define MQTTip              "mqtt.myserver.com"     //ip address or hostname of the mqtt broker
+#define MQTTport            1883                    //port of the mqtt broker
+#define MQTTuser            "client1"               //username of this mqtt client
+#define MQTTpsw             "password1"             //password of this mqtt client
+#define MQTTalive           120                     //mqtt keep alive interval (seconds)
+#define MQTTretry           10                      //time to wait before reconnect if connection drops (seconds)
+#define MQTTqos             2                       //quality of service for subscriptions and publishes
+#define timeout_check       500                     //timeout for checking incoming messages (milliseconds)
+#define timeout_send        5000                    //timeout for subscriptions and publishes (milliseconds)
+#define esp8266reset        A5                      //arduino pin connected to esp8266 reset pin (analog pin suggested due to higher impedance)
+#define esp8266alive        40                      //esp8266 keep alive interval (reset board if fail) (seconds)
 boolean connected = false;
 unsigned long lastAliveCheck;
 
-void onConnected() {	//on connected callback
-	mqttSubscribe("hello");	//subscribe to "hello" topic
+void onConnected() {                                //on connected callback
+    mqttSubscribe("hello");                         //subscribe to "hello" topic
 }
 
-void onDisconnected() {	//on disconnected callback
+void onDisconnected() {                             //on disconnected callback
 }
 
-void onMessage(String topic, String message) {	//new message callback
+void onMessage(String topic, String message) {      //new message callback
 }
 
 #### DO NOT TOUCH THIS CODE! ####
@@ -31,96 +31,96 @@ char in_buffer[buffer_l + 1];
 char cb[1];
 boolean success;
 void checkComm() {
-	if (millis() - lastAliveCheck > esp8266alive * 2000) {
-		pinMode(esp8266reset, OUTPUT);
-		delay(10);
-		pinMode(esp8266reset, INPUT);
-		lastAliveCheck = millis();
-	}
-	if (Serial.find("[(")) {
-		Serial.readBytes(cb, 1);
-		if (cb[0] == 'r') {
-			//ready
-			Serial.setTimeout(timeout_check);			
-			if (connected) {
-				connected = false;
-				onDisconnected();
-			}
-            Serial.println("startAlive(\"" + String(esp8266alive) + "\")");			
-			Serial.println("connectAP(\"" + String(APssid) + "\", \"" + String(APpsw) + "\")");
-		} else if (cb[0] == 'a') {
-				lastAliveCheck = millis();
-		} else if (cb[0] == 'w') {
-			//wifi connected
-			Serial.println("mqttInit(\"" + String(MQTTid) + "\", \"" + String(MQTTip) + "\", " + MQTTport + ", \"" + String(MQTTuser)
-							+ "\", \"" + String(MQTTpsw) + "\", " + MQTTalive + ", " + MQTTretry + ")");		
-		} else if (cb[0] == 'c') {
-			//mqtt connected
-			connected = true;
-			onConnected();
-		} else if (cb[0] == 'd') {
-			//disconnected
-			connected = false;
-			onDisconnected();
-		} else if (cb[0] == 'm') {
-			//new message
-			memset(in_buffer, 0, sizeof(in_buffer));
-			Serial.readBytesUntil('|', in_buffer, buffer_l);
-			String topic = String(in_buffer);
-			memset(in_buffer, 0, sizeof(in_buffer));
-			Serial.readBytesUntil('|', in_buffer, buffer_l);
-			String message = String(in_buffer);
+    if (millis() - lastAliveCheck > esp8266alive * 2000) {
+        pinMode(esp8266reset, OUTPUT);
+        delay(10);
+        pinMode(esp8266reset, INPUT);
+        lastAliveCheck = millis();
+    }
+    if (Serial.find("[(")) {
+        Serial.readBytes(cb, 1);
+        if (cb[0] == 'r') {
+            //ready
+            Serial.setTimeout(timeout_check);           
+            if (connected) {
+                connected = false;
+                onDisconnected();
+            }
+            Serial.println("startAlive(\"" + String(esp8266alive) + "\")");         
+            Serial.println("connectAP(\"" + String(APssid) + "\", \"" + String(APpsw) + "\")");
+        } else if (cb[0] == 'a') {
+                lastAliveCheck = millis();
+        } else if (cb[0] == 'w') {
+            //wifi connected
+            Serial.println("mqttInit(\"" + String(MQTTid) + "\", \"" + String(MQTTip) + "\", " + MQTTport + ", \"" + String(MQTTuser)
+                            + "\", \"" + String(MQTTpsw) + "\", " + MQTTalive + ", " + MQTTretry + ")");        
+        } else if (cb[0] == 'c') {
+            //mqtt connected
+            connected = true;
+            onConnected();
+        } else if (cb[0] == 'd') {
+            //disconnected
+            connected = false;
+            onDisconnected();
+        } else if (cb[0] == 'm') {
+            //new message
+            memset(in_buffer, 0, sizeof(in_buffer));
+            Serial.readBytesUntil('|', in_buffer, buffer_l);
+            String topic = String(in_buffer);
+            memset(in_buffer, 0, sizeof(in_buffer));
+            Serial.readBytesUntil('|', in_buffer, buffer_l);
+            String message = String(in_buffer);
                         while(!success)
                           checkComm();
-			onMessage(topic, message);
-		} else if (cb[0] == 'p' || cb[0] == 's') {
-			success = true;
-		}
-	}
+            onMessage(topic, message);
+        } else if (cb[0] == 'p' || cb[0] == 's') {
+            success = true;
+        }
+    }
 }
 void mqttPublish(String topic, String message, unsigned int retain) {
-  	success = false;
-	Serial.setTimeout(timeout_send);
-	while (!success) {
-			if (!connected) {
-			  success = true;
-			  break;
-			}
-		Serial.println("mqttPublish(\"" + topic + "\", \"" + message + "\",  " + MQTTqos + ", " + retain + ")");        	
-		checkComm();
-	}
-	Serial.setTimeout(timeout_check);        
+    success = false;
+    Serial.setTimeout(timeout_send);
+    while (!success) {
+            if (!connected) {
+              success = true;
+              break;
+            }
+        Serial.println("mqttPublish(\"" + topic + "\", \"" + message + "\",  " + MQTTqos + ", " + retain + ")");            
+        checkComm();
+    }
+    Serial.setTimeout(timeout_check);        
 }
 void mqttSubscribe(String topic) {
-	success = false;
-	Serial.setTimeout(timeout_send);
-	while(!success) {
-			if (!connected) {
-			  success = true;
-			  break;
-			}
-		Serial.println("mqttSubscribe(\"" + String(topic) + "\", " + MQTTqos + ")");
-		checkComm();
-	}
-	Serial.setTimeout(timeout_check);        
+    success = false;
+    Serial.setTimeout(timeout_send);
+    while(!success) {
+            if (!connected) {
+              success = true;
+              break;
+            }
+        Serial.println("mqttSubscribe(\"" + String(topic) + "\", " + MQTTqos + ")");
+        checkComm();
+    }
+    Serial.setTimeout(timeout_check);        
 }
 #### END OF UNTOUCHABLE CODE ####
 
 void setup() {
-	Serial.begin(9600);	//start serial
+    Serial.begin(9600);                                 //start serial
 
-	lastAliveCheck = millis();	//
-	while(!connected)			//
-		checkComm();			//wait for first connection
+    lastAliveCheck = millis();                          //
+    while(!connected)                                   //
+        checkComm();                                    //wait for first connection
 }
 
 void loop() {
-	do					//
-		checkComm();	//
-	while(!connected);	//check for incoming messages
-	
-	delay(10000);
-	
-	mqttPublish("uptime", String(millis()), 0);			//publish new message to "uptime" topic, with no retain
-	mqttPublish("sensor", String(analogRead(A0)), 0);	//publish new message to "sensor" topic, with retain
+    do                                                  //
+        checkComm();                                    //
+    while(!connected);                                  //check for incoming messages
+    
+    delay(10000);
+    
+    mqttPublish("uptime", String(millis()), 0);         //publish new message to "uptime" topic, with no retain
+    mqttPublish("sensor", String(analogRead(A0)), 0);   //publish new message to "sensor" topic, with retain
 }
--- mqttduino.lua
+++ mqttduino.lua
@@ -9,81 +9,81 @@ mqttId = ""
 
 local srv = net.createServer(net.TCP)
 srv:listen(80, function(conn) 
-	conn:on("receive",function(conn,payload)
-		conn:send("MQTTDUINO - Node ID: " .. node.chipid() ..
-					" - Node MAC: " .. wifi.sta.getmac() ..
-					" - MQTT status: " .. (mqttStatus == true and mqttId or 'not connected') ..
-					" - Online since: " .. tmr.time() .. "s")
-	end)
-	conn:on("sent",function(conn) conn:close() end)
+    conn:on("receive",function(conn,payload)
+        conn:send("MQTTDUINO - Node ID: " .. node.chipid() ..
+                    " - Node MAC: " .. wifi.sta.getmac() ..
+                    " - MQTT status: " .. (mqttStatus == true and mqttId or 'not connected') ..
+                    " - Online since: " .. tmr.time() .. "s")
+    end)
+    conn:on("sent",function(conn) conn:close() end)
 end)
 
 mqttInit = function (id, ip, port, user, password, alive, retry)
-	mqttId = id;
-	mqttIp = ip
-	mqttPort = port
-	mqttRetry = retry * 1000
-	
-	m = mqtt.Client(mqttId, alive, user, password)
-	m:lwt(mqttId .. "/status", "0", 2, 1)
+    mqttId = id;
+    mqttIp = ip
+    mqttPort = port
+    mqttRetry = retry * 1000
+    
+    m = mqtt.Client(mqttId, alive, user, password)
+    m:lwt(mqttId .. "/status", "0", 2, 1)
 
-	m:on("offline", function(conn)
-		if mqttStatus then
-			print("[(d")
-		end
-		mqttStatus = false
-		tmr.alarm(0, mqttRetry, 1, function() 
-			if mqttStatus then
-				tmr.stop(0)
-			else
-				mqttConnect()
-			end
-		end)
-	end)
-	m:on("message", function(conn, topic, data)
-		if data ~= nil then
-			print("[(m" .. topic .. "|" .. data .. "|")
-		end
-	end)
-	
-	mqttConnect()
+    m:on("offline", function(conn)
+        if mqttStatus then
+            print("[(d")
+        end
+        mqttStatus = false
+        tmr.alarm(0, mqttRetry, 1, function() 
+            if mqttStatus then
+                tmr.stop(0)
+            else
+                mqttConnect()
+            end
+        end)
+    end)
+    m:on("message", function(conn, topic, data)
+        if data ~= nil then
+            print("[(m" .. topic .. "|" .. data .. "|")
+        end
+    end)
+    
+    mqttConnect()
 end
 
 mqttConnect = function ()
-	m:connect(mqttIp, mqttPort, 0, function(conn)
-		m:publish(mqttId .. "/status", "1", 2, 1, function(conn)
-				print("[(c")
-				mqttStatus = true
-			end)
-	end)
+    m:connect(mqttIp, mqttPort, 0, function(conn)
+        m:publish(mqttId .. "/status", "1", 2, 1, function(conn)
+            print("[(c")
+            mqttStatus = true
+        end)
+    end)
 end
 
 mqttSubscribe = function (topic, qos)
-	m:subscribe(topic, qos, function(conn) print("[(s") end)
+    m:subscribe(topic, qos, function(conn) print("[(s") end)
 end
 
 mqttPublish = function (topic, message, qos, ret)
-	m:publish(topic, message, qos, ret, function(conn) print("[(p") end)
+    m:publish(topic, message, qos, ret, function(conn) print("[(p") end)
 end
 
 connectAP = function (ssid, password)
-	wifi.setmode(wifi.STATION)
-	wifi.sta.config(ssid, password)
-	tmr.alarm(0, 1000, 1, function()
-		if wifi.sta.getip() ~= nil then
-			tmr.stop(0)
-			print("[(w");
-		end
-	end)	
+    wifi.setmode(wifi.STATION)
+    wifi.sta.config(ssid, password)
+    tmr.alarm(0, 1000, 1, function()
+        if wifi.sta.getip() ~= nil then
+            tmr.stop(0)
+            print("[(w");
+        end
+    end)    
 end
 
 startAlive = function (interval)
-	tmr.alarm(1, interval * 1000, 1, function()
-		print("[(a")
-	end)
+    tmr.alarm(1, interval * 1000, 1, function()
+        print("[(a")
+    end)
 end
 
 tmr.alarm(0, 10000, 1, function()
-	tmr.stop(0)
-	print("[(r")
+    tmr.stop(0)
+    print("[(r")
 end)
